cbb2df26ee55ea8634ad8b584082658a230e41b8
JoaoFilipeMartinsDeOliveira <joaofefe22@gmail.com>
2025-07-25 03:54:10 -0300

    Essa bosta
    Tem como fazer essa porcaria não

---
+++ abelhasgames/character_body_2d.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+const SPEED = 300;
+const JUMP_VELOCITY = -400;
+@onready var CAMERAVIEWSIZE = get_viewport().size.y
+@onready var CAMERA: Camera2D = $Camera2D
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	if Input.is_action_just_pressed("Espaco") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	var direction = Input.get_axis("A", "D")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	move_and_slide()
+		
+	
+	CAMERA.drag_vertical_offset = CAMERAVIEWSIZE
+	#print(CAMERAVIEWSIZE*0.33)
+	print(get_global_mouse_position())
